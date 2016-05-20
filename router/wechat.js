17e4a40269a141bbe8a72a5958068eac9226df6c
zouzhenxing <zouzx@yunku.org>
2016-05-20 14:42:22 +0800

    增加网站支持

--- router/wechat.js
+++ router/wechat.js
@@ -1,9 +1,22 @@
 var wechat = require('wechat');
 
 exports.wechat = wechat('yuankuorg').text(function (message, req, res, next) {
-  res.reply({type: "text", content: "源酷小精灵说：" + message });
+  if( message.Content == '买' ) {
+      res.reply([
+        {
+            title: '这是一个神奇网站',
+            description: '点击进入可以购卖东西',
+            url: 'http://115.28.229.151/weweb/index.html'
+        }
+      ]);
+  }
 }).image(function (message, req, res, next) {
-  // TODO
+  res.reply({
+    type: "image",
+    content: {
+        mediaId: message.MediaId
+    }
+  });
 }).voice(function (message, req, res, next) {
   // TODO
 }).video(function (message, req, res, next) {
